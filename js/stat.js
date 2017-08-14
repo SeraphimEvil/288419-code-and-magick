7bac669cbf919651b480a451ebd0fe49a489294e
Sergey Ostapovec <seraphimaion@gmail.com>
2017-08-14 23:51:19 +0300

    правки по комментариям

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.renderStatistics = function (ctx, names, times) {
+var createResultField = (ctx) => {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.strokeStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.beginPath();
@@ -21,31 +21,37 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.lineTo(100, 280);
   ctx.closePath();
   ctx.fill();
+};
 
-
+var typeMessageOnField = (ctx) => {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.font = '16px PT Mono';
 
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
+  var GZ_MESSAGE = 'Ура вы победили!';
+  var RESULT_LIST_MESSAGE = 'Список результатов:';
 
-  var max = -1;
+  ctx.fillText(GZ_MESSAGE, 120, 40);
+  ctx.fillText(RESULT_LIST_MESSAGE, 120, 60);
+};
 
-  for (var j = 0; j < times.length; j++) {
-    var time = times[j];
-    if (time > max) {
-      max = time;
-    }
-  }
+var findMax = (times) => {
+  return Math.max.apply(null, times);
+};
 
-  var histogramWidth = 40;
-  var barHeigth = 150;
-  var step = barHeigth / (max - 0);
-  var indent = 50 + histogramWidth;
-  var initialX = 150;
-  var initialY = 250;
-  var resultViewСoefficient = 20;
+window.renderStatistics = function (ctx, names, times) {
+  createResultField(ctx);
+  typeMessageOnField(ctx);
+
+  var max = findMax(times);
+  var HISTOGRAM_WIDTH = 40;
+  var HISTOGRAM_HEIGHT = 150;
+  var INDENT = 50 + HISTOGRAM_WIDTH;
+  var INITIAL_X = 150;
+  var INITIAL_Y = 250;
+  var RESULT_VIEW_COEFFICIENT = 20;
+
   var randomOpacity;
+  var step = HISTOGRAM_HEIGHT / (max - 0);
 
 
   for (var i = 0; i < times.length; i++) {
@@ -54,8 +60,8 @@ window.renderStatistics = function (ctx, names, times) {
 
     ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
 
-    ctx.fillText(names[i], initialX + indent * i, initialY + resultViewСoefficient);
-    ctx.fillText(times[i], initialX + indent * i, (step * times[i]) - resultViewСoefficient);
+    ctx.fillText(names[i], INITIAL_X + INDENT * i, INITIAL_Y + RESULT_VIEW_COEFFICIENT);
+    ctx.fillText(times[i], INITIAL_X + INDENT * i, (step * times[i]) - RESULT_VIEW_COEFFICIENT);
 
     ctx.fillStyle = 'rgba(0, 0, 255, ' + randomOpacity + ')';
 
@@ -64,10 +70,10 @@ window.renderStatistics = function (ctx, names, times) {
     }
 
     ctx.beginPath();
-    ctx.moveTo(initialX + indent * i, initialY);
-    ctx.lineTo((initialX + histogramWidth) + indent * i, initialY);
-    ctx.lineTo((initialX + histogramWidth) + indent * i, step * times[i]);
-    ctx.lineTo(initialX + indent * i, step * times[i]);
+    ctx.moveTo(INITIAL_X + INDENT * i, INITIAL_Y);
+    ctx.lineTo((INITIAL_X + HISTOGRAM_WIDTH) + INDENT * i, INITIAL_Y);
+    ctx.lineTo((INITIAL_X + HISTOGRAM_WIDTH) + INDENT * i, step * times[i]);
+    ctx.lineTo(INITIAL_X + INDENT * i, step * times[i]);
     ctx.closePath();
     ctx.fill();
   }
